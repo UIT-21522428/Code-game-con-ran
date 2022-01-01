eaa9e79bd1f33049cc5330b70baae980d57a7498
UIT21522155 <21522155@gm.uit.edu.vn>
2022-01-01 15:25:33 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,1 +1,30 @@
 
+
+
+struct Point
+{
+	int x;
+	int y;
+};
+
+vector<Point> snake = {
+	Point{ WIDTH / 2 + 2, HEIGHT / 2 },
+	Point{ WIDTH / 2 + 1, HEIGHT / 2 },
+	Point{ WIDTH / 2, HEIGHT / 2 },
+	Point{ WIDTH / 2 - 1, HEIGHT / 2 },
+	Point{ WIDTH / 2 - 2, HEIGHT / 2 }
+};
+
+
+void drawSnakePart(Point p)
+{
+	gotoxy(p.x, p.y);
+	// BODY là ký tự đại diện cho phần thân của con rắn
+	cout << BODY;
+}
+
+void drawSnake()
+{
+	for (size_t i = 0; i < snake.size(); i++)
+		drawSnakePart(snake[i]);
+}
