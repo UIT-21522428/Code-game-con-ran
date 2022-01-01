515b628a61b0be688cd43232a6d96105a82ad7b1
UIT-21522539 <96250317+UIT21522539@users.noreply.github.com>
2022-01-01 13:58:57 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,1 +1,18 @@
+Point prevTail;
 
+void move()
+{
+	// lưu phần đuôi cũ lại
+	prevTail = snake.back();
+	// code gores here
+}
+
+void drawHeadnTail()
+{
+	gotoxy(snake[0].x, snake[0].y);
+	cout << BODY;
+ // vẽ phần đầu mới
+	Point tail = snake.back();
+	gotoxy(prevTail.x, prevTail.y);
+	cout << ' '; // xóa phần đuôi cũ đi
+}
