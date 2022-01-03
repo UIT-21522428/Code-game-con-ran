3843b1ace297ae12ac02b1797f502e65f2c78958
UIT-21522076 <96250708+21522076uit@users.noreply.github.com>
2022-01-03 07:37:31 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,32 +1,18 @@
 //tạo quả táo 
-
-void genApple()
+void tao_do_an()
 {
 	srand(time(0));
-	int x = rand() % (WIDTH - 1) + 1;
-	int y = rand() % (HEIGHT - 1) + 1;
-	apple = {
+	int x = rand() % (C_RONG - 1) + 1;
+	int y = rand() % (C_CAO - 1) + 1;
+	do_an = {
 		x,
 		y,
 	};
-	// Sau khi có tọa độ quả táo thì vẽ lên màn hình
 	gotoxy(x, y);
-	cout << APPLE;
-}
-//kiểm tra quả táo có được ăn không?
-
-bool isAteApple()
-{
-	return snake[0].x == apple.x && snake[0].y == apple.y;
-}
-//tăng chiều dài con rắn
-void growing()
-{
-	snake.push_back(prevTail);
+	cout << DO_AN;
 }
 
-if (isAteApple())
+bool an_do_an()
 {
-	growing();
-	genApple();
+	return snake[0].x == do_an.x && snake[0].y == do_an.y;
 }
