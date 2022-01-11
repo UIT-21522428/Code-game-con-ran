d622ae7d32f1f87755d953148a19f0c3d4472fc9
UIT21522474 <96251720+UIT21522474@users.noreply.github.com>
2022-01-11 21:56:26 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -5,13 +5,3 @@ void ve_dau_duoi()
 	gotoxy(duoi_truoc.x, duoi_truoc.y);
 	cout << ' '; // Clear the old tail
 }
-
-bool can_ban_than()
-{
-	phan_than head = snake[0];
-	for (size_t i = 1; i < snake.size(); i++)
-		if (head.x == snake[i].x && head.y == snake[i].y)
-			return true;
-	return false;
-}
-
