d4dcf0bc5f6e138e2c50bd9b6ba0b53f7578edff
UIT-21522539 <96250317+UIT21522539@users.noreply.github.com>
2022-01-13 15:40:06 +0700

    Update code.cpp

--- code.cpp
+++ code.cpp
@@ -1,11 +1,11 @@
+void move()
+{
+	duoi_truoc = snake.back();
+}
 void ve_dau_duoi()
 {
 	gotoxy(snake[0].x, snake[0].y);
 	cout << THAN;
 	gotoxy(duoi_truoc.x, duoi_truoc.y);
 	cout << ' '; // Clear the old tail
-}
-void map_ra()
-{
-    snake.push_back(duoi_truoc);
 }
