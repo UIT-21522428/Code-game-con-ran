bf955c1180b6eec7077bf2b495501032c9f5e120
UIT-21522428 <96250312+UIT-21522428@users.noreply.github.com>
2022-01-13 10:51:05 +0700

    them dung tuong o map 2

--- code.cpp
+++ code.cpp
@@ -18,6 +18,23 @@ bool dung_tuong()
 {
 	return snake[0].x == 0 || snake[0].y == 0 || snake[0].x == C_RONG || snake[0].y == C_CAO;
 }
+bool dung_tuong_crush() //logic khi ran dung tuong o map 2
+{
+	return snake[0].x == 0 || snake[0].y == 0
+	|| snake[0].x == C_RONG || snake[0].y == C_CAO
+	|| (snake[0].x == 2 && 4 <= snake[0].y && snake[0].y <= 8)
+	|| (snake[0].x == 8 && 5 <= snake[0].y && snake[0].y <= 6)
+	|| (snake[0].x == 9 && 4 <= snake[0].y && snake[0].y <= 7)
+	|| (snake[0].x == 10 && 5 <= snake[0].y && snake[0].y <= 8)
+	|| (snake[0].x == 11 && 6 <= snake[0].y && snake[0].y <= 8)
+	|| (snake[0].x == 12 && 5 <= snake[0].y && snake[0].y <= 8)
+	|| (snake[0].x == 13 && 4 <= snake[0].y && snake[0].y <= 7)
+	|| (snake[0].x == 14 && 5 <= snake[0].y && snake[0].y <= 6)
+	|| (snake[0].x == 18 && 4 <= snake[0].y && snake[0].y <= 7)
+	|| (snake[0].x == 22 || 4 <= snake[0].y && snake[0].y <= 7)
+	|| (snake[0].y == 8 && 18 <= snake[0].x && snake[0].x <= 22)
+	;
+}
 //can ban than
 bool can_ban_than()
 {
@@ -27,15 +44,3 @@ bool can_ban_than()
 			return true;
 	return false;
 }
-
-//them lai
-move();
-		
-		if (can_ban_than())
-		{
-			break;
-		}
-		if (dung_tuong())
-		{
-			break;
-		}
